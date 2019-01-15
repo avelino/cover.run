5df50bdd424fb523611c41846fd51cd4097fafda
Chase Hutchins <syntaqx@gmail.com>
2019-01-15 07:48:07 -0700

    lowercase golang

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "Building Go images required for testing coverage"
-cp ./dockers/Golang/run.sh ./dockers/Golang/1.11/run.sh
-docker build --tag avelino/cover.run:golang-1.11 ./dockers/Golang/1.11
-rm ./dockers/Golang/1.11/run.sh
+cp ./dockers/golang/run.sh ./dockers/golang/1.11/run.sh
+docker build --tag avelino/cover.run:golang-1.11 ./dockers/golang/1.11
+rm ./dockers/golang/1.11/run.sh
 
-cp ./dockers/Golang/run.sh ./dockers/Golang/1.10/run.sh
-docker build --tag avelino/cover.run:golang-1.10 ./dockers/Golang/1.10
-rm ./dockers/Golang/1.10/run.sh
+cp ./dockers/golang/run.sh ./dockers/golang/1.10/run.sh
+docker build --tag avelino/cover.run:golang-1.10 ./dockers/golang/1.10
+rm ./dockers/golang/1.10/run.sh
 
-cp ./dockers/Golang/run.sh ./dockers/Golang/1.9/run.sh
-docker build --tag avelino/cover.run:golang-1.9 ./dockers/Golang/1.9
-rm ./dockers/Golang/1.9/run.sh
+cp ./dockers/golang/run.sh ./dockers/golang/1.9/run.sh
+docker build --tag avelino/cover.run:golang-1.9 ./dockers/golang/1.9
+rm ./dockers/golang/1.9/run.sh
 
-cp ./dockers/Golang/run.sh ./dockers/Golang/1.8/run.sh
-docker build --tag avelino/cover.run:golang-1.8 ./dockers/Golang/1.8
-rm ./dockers/Golang/1.8/run.sh
+cp ./dockers/golang/run.sh ./dockers/golang/1.8/run.sh
+docker build --tag avelino/cover.run:golang-1.8 ./dockers/golang/1.8
+rm ./dockers/golang/1.8/run.sh
