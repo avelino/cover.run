3a630658ba5f80f6a7bb6d554c86ba6a701bf071
Chase Hutchins <syntaqx@gmail.com>
2019-01-15 08:25:38 -0700

    trying a new directory

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 echo "Building Go images required for testing coverage"
-cp ./dockers/golang/run.sh ./dockers/golang/1.11/run.sh
-docker build --tag avelino/cover.run:golang-1.11 ./dockers/golang/1.11
-rm ./dockers/golang/1.11/run.sh
+cp ./build/docker/golang/run.sh ./build/docker/golang/1.11/run.sh
+docker build --tag avelino/cover.run:golang-1.11 ./build/docker/golang/1.11
+rm ./build/docker/golang/1.11/run.sh
 
-cp ./dockers/golang/run.sh ./dockers/golang/1.10/run.sh
-docker build --tag avelino/cover.run:golang-1.10 ./dockers/golang/1.10
-rm ./dockers/golang/1.10/run.sh
+cp ./build/docker/golang/run.sh ./build/docker/golang/1.10/run.sh
+docker build --tag avelino/cover.run:golang-1.10 ./build/docker/golang/1.10
+rm ./build/docker/golang/1.10/run.sh
 
-cp ./dockers/golang/run.sh ./dockers/golang/1.9/run.sh
-docker build --tag avelino/cover.run:golang-1.9 ./dockers/golang/1.9
-rm ./dockers/golang/1.9/run.sh
+cp ./build/docker/golang/run.sh ./build/docker/golang/1.9/run.sh
+docker build --tag avelino/cover.run:golang-1.9 ./build/docker/golang/1.9
+rm ./build/docker/golang/1.9/run.sh
 
-cp ./dockers/golang/run.sh ./dockers/golang/1.8/run.sh
-docker build --tag avelino/cover.run:golang-1.8 ./dockers/golang/1.8
-rm ./dockers/golang/1.8/run.sh
+cp ./build/docker/golang/run.sh ./build/docker/golang/1.8/run.sh
+docker build --tag avelino/cover.run:golang-1.8 ./build/docker/golang/1.8
+rm ./build/docker/golang/1.8/run.sh
